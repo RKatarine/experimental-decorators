9f9e65e580d22b1b39f504a2e5e14e41535b63d3
RKatarine <rkatarine@mail.ru>
2020-08-07 12:19:33 +0300

    more examples

--- decorator/class.ts
+++ decorator/class.ts
@@ -1,9 +1,27 @@
 import "reflect-metadata";
 import {logType} from "./logType";
 
+interface IParams {
+    data: string;
+}
 
 class Demo {
 
     @logType // apply property decorator
-    public attr1: string;
+    public attr1!: string;
+
+    @logType
+    setOptions = ({params, str}: { params: IParams, str: string }) => {
+        this.attr1 = params.data;
+    }
+
+    @logType
+    setOptionsNext = (params: IParams) => {
+        this.attr1 = params.data;
+    }
+
+    @logType
+    setOptionsNextLext = (str: string) => {
+        this.attr1 = str;
+    }
 }
